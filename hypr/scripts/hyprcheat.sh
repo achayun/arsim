604ecad68885a1f3ec40b44b30db200615b14bd0
Ami Chayun <amic@aftershock.co.il>
2026-01-15 23:32:42 +0900

    Cheatsheet and some organization

---
+++ hypr/scripts/hyprcheat.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+HYPRCONF="$HOME/.config/hypr/hyprland.conf"
+TIMEOUT=60
+
+# TODO: Parse from config
+MAINMOD=""
+get_binds() {
+	grep '^bindd' "$HYPRCONF" | sed 's/^bindd = //g' | while read -r line; do
+		MOD=$(echo "$line" | cut -d',' -f1 | xargs | sed "s/\$mainMod\s*/$MAINMOD+/")
+		KEY=$(echo "$line" | cut -d',' -f2 | xargs)
+		DESC=$(echo "$line" | cut -d',' -f3 | xargs)
+		CMD=$(echo "$line" | cut -d',' -f4 | xargs)
+		APP=$(echo "$line" | cut -d',' -f5 | xargs)
+		echo -e "$MOD+$KEY\n$DESC\n($CMD $APP)"
+	done
+}
+
+(
+	echo -e "ESC\nclose this cheatsheet\n"
+	get_binds
+	echo -e "\n\n     Window close in $TIMEOUT sec.";
+) | yad \
+	--fixed \
+	--width=600 --height=700 --center \
+	--title="Hyprland Keybindings" \
+	--no-buttons \
+	--list \
+	--column="Key" --column="Description" --column="Command" \
+	--timeout=$TIMEOUT --timeout-indicator=right \
+	--separator="\n" \
+	--search-column=2
